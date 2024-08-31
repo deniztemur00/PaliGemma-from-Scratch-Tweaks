ed2c2a3b950e793933ef4daa2486752f978b92dc
Deniz <56404667+Schiweppes@users.noreply.github.com>
2024-08-31 18:17:27 +0300

    Grad scaler and bfloat16 implementation for training

--- pali_gemma.py
+++ pali_gemma.py
@@ -402,9 +402,8 @@ class PaliGemmaForConditionalGen(nn.Module):
         self.multi_modal_projector = PaliGemmaMultiModalProjector(config)
         self.vocab_size = config.vocab_size
 
-        language_model = GemmaForCausalLM(config.text_config)
+        self.language_model = GemmaForCausalLM(config.text_config)
 
-        self.language_model = language_model
 
         self.pad_token_id = (
             self.config.pad_token_id if self.config.pad_token_id is not None else -1
